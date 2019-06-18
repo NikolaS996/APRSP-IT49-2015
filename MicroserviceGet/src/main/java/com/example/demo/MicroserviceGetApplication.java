705521ef4fa5244ff514dcb4104096e5db4b3063
Nikola Savic <nikolasavic1996@live.com>
2019-06-19 01:38:38 +0200

    home to localhost

--- MicroserviceGet/src/main/java/com/example/demo/MicroserviceGetApplication.java
+++ MicroserviceGet/src/main/java/com/example/demo/MicroserviceGetApplication.java
@@ -2,10 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @EnableDiscoveryClient
+@EnableRetry
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class},scanBasePackages={
 		"com.example.demo.reps.CompanyRepository", "com.example.application"})
 public class MicroserviceGetApplication {
