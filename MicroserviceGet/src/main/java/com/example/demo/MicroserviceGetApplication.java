50b8abe8f7358ce98459cacd926a2d54c69c3da1
Nikola Savic <nikolasavic1996@live.com>
2019-06-19 03:18:51 +0200

    fix 3.0

--- MicroserviceGet/src/main/java/com/example/demo/MicroserviceGetApplication.java
+++ MicroserviceGet/src/main/java/com/example/demo/MicroserviceGetApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @EnableDiscoveryClient
 @EnableRetry
+//@EnableEurekaClient
 //@SpringBootApplication(exclude={DataSourceAutoConfiguration.class},scanBasePackages={
 //		"com.example.demo.reps.CompanyRepository", "com.example.application"})
 @SpringBootApplication
